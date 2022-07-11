edcfdc4f70f446ed275eacc01635e49526b85b15
Riya Ghosh <107564132+riyaghosh123@users.noreply.github.com>
2022-07-11 09:47:27 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,15 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");
+     System.out.print("hello java");
+     System.out.print("hello java\n");
+     System.out.println("hello java with me\nhello world");  
+     
+     System.out.println("*");
+     System.out.println("* *");
+     System.out.println("* * *");
+     System.out.println("* * * *");
+     System.out.print("*\n**\n***\n****");
+
+    }  
+}
